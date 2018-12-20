a63215f46c7066a11f5b5a43026f6cb7087cf677
Ritsard-MBP <chardeefaye@gmail.com>
2018-12-20 19:05:13 +0800

    Upgraded to Act 6

--- app/src/main/java/com/applications/roleosala/leosala/gameStart.java
+++ app/src/main/java/com/applications/roleosala/leosala/gameStart.java
@@ -1,37 +1,56 @@
 package com.applications.roleosala.leosala;
 
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class gameStart extends AppCompatActivity {
-    Button a1, a2, b1, b2, c1,c2,d1,d2,e1,e2,f1,f2,g1,g2,h1,h2;
+    Button a1, a2, b1, b2, c1,c2,d1,d2,e1,e2,f1,f2,g1,g2,h1,h2, i1, i2, j1, j2,k1, k2,l1,l2, btnBonus;
     TextView txtCorrect, txtIncorrect, txtTime;
-
+    Button buttons [] = new Button[24];
+    String alpha[] = new String[24];
+    ArrayList<Integer>  number;
     @Override
     protected void onStart() {
         super.onStart();
         CountDownTimer countDownTimer;
-        new CountDownTimer(3000, 1000) {
-            int time = 3;
+        new CountDownTimer(5000, 1000) {
+            int time = 5;
             @Override
             public void onTick(long millisUntilFinished) {
                 time -= 1;
                 txtTime.setText(String.valueOf(time));
+                for(int i = 0; i < buttons.length; i++){
+                    change(1, buttons[i]);
+                    buttons[i].setText(String.valueOf(alpha[number.get(i)]));
+                }
             }
 
             @Override
             public void onFinish() {
-
+                for(int i = 0; i < buttons.length; i++){
+                    change(0, buttons[i]);
+                }
+                for(int i = 0; i < buttons.length; i++){
+                    buttons[i].setText(R.string.btn2ndAct);
+                }
             }
         }.start();
+    }
+
+    public void change(int i, View v){
+        if(i == 0){
+            v.setEnabled(true);
+        }else{
+            v.setEnabled(false);
+        }
     }
 
     @Override
@@ -39,13 +58,10 @@ public class gameStart extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_start);
         __init__();
-        exqListener();
+        for (int j = 0; j < buttons.length; j++){
+            buttons[j].setText(alpha[number.get(j)]);
+        }
     }
-
-    private void exqListener() {
-
-    }
-
     private void __init__() {
         txtCorrect = findViewById(R.id.txtCorrect);
         txtIncorrect = findViewById(R.id.txtIncorrect);
@@ -66,79 +82,39 @@ public class gameStart extends AppCompatActivity {
         g2 = findViewById(R.id.g2);
         h1 = findViewById(R.id.h1);
         h2 = findViewById(R.id.h2);
+        i1 = findViewById(R.id.i1);
+        i2 = findViewById(R.id.i2);
+        j1 = findViewById(R.id.j1);
+        j2 = findViewById(R.id.j2);
+        k1 = findViewById(R.id.k1);
+        k2 = findViewById(R.id.k2);
+        l1 = findViewById(R.id.l1);
+        l2 = findViewById(R.id.l2);
+        buttons = new Button[]{a1, a2, b1, b2, c1,c2,d1,d2,e1,e2,f1,f2,g1,g2,h1,h2, i1, i2, j1, j2,k1, k2,l1,l2};
+        number = new ArrayList<>();
+        for (int i=0; i < buttons.length; i++){
+            number.add(i);
+        }
+        alpha = getResources().getStringArray(R.array.alphabet);
+        Collections.shuffle(number);
+        for (int i = 0; i < number.size(); i++){
+            Log.d(String.valueOf(i), String.valueOf(number.get(i)));
+        }
+        for(int i = 0; i < alpha.length; i++){
+            Log.d(String.valueOf(i), alpha[i]);
+        }
     }
-    String prevStr;
-    String curStr;
+
+    String prevStr = null;
+    String curStr = null;
     int c = 0;
 
     public void setName(View v){
-        switch (v.getId()){
-            case R.id.a1:
-                a1.setText("A");
-                check("A", 1, a1);
-                break;
-            case R.id.a2:
-                a2.setText("A");
-                check("A", 1, a2);
-                break;
-            case R.id.b1:
-                b1.setText("B");
-                check("B", 1, b1);
-                break;
-            case  R.id.b2:
-                b2.setText("B");
-                check("B", 1,b2);
-                break;
-            case R.id.c1:
-                c1.setText("C");
-                check("C", 1, c1);
-                break;
-            case R.id.c2:
-                c2.setText("C");
-                check("C", 1, c2);
-                break;
-            case R.id.d1:
-                d1.setText("D");
-                check("D", 1, d1);
-                break;
-            case R.id.d2:
-                d2.setText("D");
-                check("D", 1, d2);
-                break;
-            case R.id.e1:
-                e1.setText("E");
-                check("F", 1, e1);
-                break;
-            case R.id.e2:
-                e2.setText("E");
-                check("F", 1, e2);
-                break;
-            case  R.id.f1:
-                f1.setText("F");
-                check("F", 1, f1);
-                break;
-            case R.id.f2:
-                f2.setText("F");
-                check("F", 1, f2);
-                break;
-            case R.id.g1:
-                g1.setText("G");
-                check("G", 1, g1);
-                break;
-            case R.id.g2:
-                g2.setText("G");
-                check("G", 1, g2);
-                break;
-            case R.id.h1:
-                h1.setText("H");
-                check("H", 1, h1);
-                break;
-            case R.id.h2:
-                h2.setText("H");
-                check("H", 1, h2);
-                break;
-                default:
-                    break;
+        for(int i = 0; i <buttons.length; i++){
+            if(v.getId() == buttons[i].getId()){
+                buttons[i].setText(alpha[number.get(i)]);
+                check(alpha[number.get(i)], 1, v);
+            }
         }
     }
 
@@ -146,177 +122,56 @@ public class gameStart extends AppCompatActivity {
     View buttonId [] = new View[2];
 
     public void check(String let, int count, View v){
-        this.c += count;
+        c += count;
         if(c == 1){
             prevStr = let;
             buttonId[0] = v;
         }else if (c == 2){
             curStr = let;
             buttonId[1] = v;
-        }else{
-            c = 0;
-        }
-
-        if(c == 2){
-            if (prevStr == curStr){
+            if (prevStr.contains(curStr)){
                 correct += 1;
                 txtCorrect.setText(String.valueOf(correct));
-                turnOff(let);
-            }else{
+                turnOff(buttonId[0], buttonId[1]);
+                buttonId[0] = null;
+                buttonId[1] = null;
+                c = 0;
+            }else {
                 incorrect += 1;
                 txtIncorrect.setText(String.valueOf(incorrect));
-                turnBack(buttonId[0], buttonId[1]);
+                Handler handler = new Handler();
+                for(int i = 0; i < buttons.length; i++){
+                    change(1, buttons[i]);
+                }
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        turnBack(buttonId[0], buttonId[1]);
+                        for(int i = 0; i < buttons.length; i++){
+                            change(0, buttons[i]);
+                        }
+                    }
+                }, 2000);
+                c=0;
             }
-            c = 0;
         }
-        Log.d("Value of C", String.valueOf(c));
-        Log.d("Correct", String.valueOf(correct));
-        Log.d("Incorrect", String.valueOf(incorrect));
     }
 
     private void turnBack( View v1, View v2) {
-        switch (v1.getId()){
-            case R.id.a1:
-                a1.setText("???");
-                break;
-            case R.id.a2:
-                a2.setText("???");
-                break;
-            case R.id.b1:
-                b1.setText("???");
-                break;
-            case  R.id.b2:
-                b2.setText("???");
-                break;
-            case R.id.c1:
-                c1.setText("???");
-                break;
-            case R.id.c2:
-                c2.setText("???");
-                break;
-            case R.id.d1:
-                d1.setText("???");
-                break;
-            case R.id.d2:
-                d2.setText("???");
-                break;
-            case R.id.e1:
-                e1.setText("???");
-                break;
-            case R.id.e2:
-                e2.setText("???");
-                break;
-            case  R.id.f1:
-                f1.setText("???");
-                break;
-            case R.id.f2:
-                f2.setText("???");
-                break;
-            case R.id.g1:
-                g1.setText("???");
-                break;
-            case R.id.g2:
-                g2.setText("???");
-                break;
-            case R.id.h1:
-                h1.setText("???");
-                break;
-            case R.id.h2:
-                h2.setText("???");
-                break;
-            default:
-                break;
-        }
-        switch (v2.getId()){
-            case R.id.a1:
-                a1.setText("???");
-                break;
-            case R.id.a2:
-                a2.setText("???");
-                break;
-            case R.id.b1:
-                b1.setText("???");
-                break;
-            case  R.id.b2:
-                b2.setText("???");
-                break;
-            case R.id.c1:
-                c1.setText("???");
-                break;
-            case R.id.c2:
-                c2.setText("???");
-                break;
-            case R.id.d1:
-                d1.setText("???");
-                break;
-            case R.id.d2:
-                d2.setText("???");
-                break;
-            case R.id.e1:
-                e1.setText("???");
-                break;
-            case R.id.e2:
-                e2.setText("???");
-                break;
-            case  R.id.f1:
-                f1.setText("???");
-                break;
-            case R.id.f2:
-                f2.setText("???");
-                break;
-            case R.id.g1:
-                g1.setText("???");
-                break;
-            case R.id.g2:
-                g2.setText("???");
-                break;
-            case R.id.h1:
-                h1.setText("???");
-                break;
-            case R.id.h2:
-                h2.setText("???");
-                break;
-            default:
-                break;
+        for(int i = 0; i < buttons.length; i++){
+            if(v1.getId() == buttons[i].getId()){
+                buttons[i].setText(getResources().getText(R.string.btn2ndAct));
+            }
+            if(v2.getId() == buttons[i].getId()){
+                buttons[i].setText(getResources().getText(R.string.btn2ndAct));
+            }
         }
     }
 
 
-    private void turnOff(String let) {
-        switch (let){
-            case "A":
-                a1.setEnabled(false);
-                a2.setEnabled(false);
-                break;
-            case "B":
-                b1.setEnabled(false);
-                b2.setEnabled(false);
-                break;
-            case "C":
-                c1.setEnabled(false);
-                c2.setEnabled(false);
-                break;
-            case "D":
-                d1.setEnabled(false);
-                d2.setEnabled(false);
-                break;
-            case "E":
-                e1.setEnabled(false);
-                e2.setEnabled(false);
-                break;
-            case "F":
-                f1.setEnabled(false);
-                f2.setEnabled(false);
-                break;
-            case "G":
-                g1.setEnabled(false);
-                g2.setEnabled(false);
-                break;
-            case "H":
-                h1.setEnabled(false);
-                h2.setEnabled(false);
-                break;
-        }
+    private void turnOff(View v1, View v2) {
+        v1.setEnabled(false);
+        v2.setEnabled(false);
     }
 
 }
